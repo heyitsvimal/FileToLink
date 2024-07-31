4e67fcc014ab2e86d02d4825aed26ed91053c29c
hey it's vimal <152893256+heyitsvimal@users.noreply.github.com>
2024-07-31 14:50:22 +0530

    Update vars.py

--- Adarsh/vars.py
+++ Adarsh/vars.py
@@ -9,23 +9,23 @@ load_dotenv()
 
 class Var(object):
     MULTI_CLIENT = False
-    API_ID = int(getenv('API_ID', '20389440'))
-    API_HASH = str(getenv('API_HASH', 'a1a06a18eb9153e9dbd447cfd5da2457'))
-    BOT_TOKEN = str(getenv('BOT_TOKEN', ''))
-    name = str(getenv('name', 'filetolinkvjbot'))
+    API_ID = int(getenv('API_ID', '29875875'))
+    API_HASH = str(getenv('API_HASH', '7d4d5740655274974b56c54ca0e2080e'))
+    BOT_TOKEN = str(getenv('BOT_TOKEN', '7064269814:AAHpquop5v-FMqOsTmDPsjZL7mX8cMJsos0'))
+    name = str(getenv('name', 'Tnlinkshortnet_bot'))
     SLEEP_THRESHOLD = int(getenv('SLEEP_THRESHOLD', '60'))
     WORKERS = int(getenv('WORKERS', '4'))
-    BIN_CHANNEL = int(getenv('BIN_CHANNEL', '-1001935365147'))
+    BIN_CHANNEL = int(getenv('BIN_CHANNEL', '-1002217005166'))
     PORT = int(getenv('PORT', 8080))
     BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
     PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
-    OWNER_ID = set(int(x) for x in os.environ.get("OWNER_ID", "6168162777").split())  
+    OWNER_ID = set(int(x) for x in os.environ.get("OWNER_ID", "1516235597").split())  
     NO_PORT = bool(getenv('NO_PORT', False))
     APP_NAME = None
-    OWNER_USERNAME = str(getenv('OWNER_USERNAME', 'kingvj01'))
+    OWNER_USERNAME = str(getenv('OWNER_USERNAME', 'heyitsvimal'))
     if 'DYNO' in environ:
         ON_HEROKU = True
-        APP_NAME = str(getenv('APP_NAME'))
+        APP_NAME = str(getenv('tnlinkshortner'))
     
     else:
         ON_HEROKU = False
